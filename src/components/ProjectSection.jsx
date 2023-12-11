9202dc502d03ef00a45d94de4200d90e86ff67fa
ace malto <maltoace9@gmail.com>
2023-12-11 22:33:11 +0800

    projectSec

--- src/components/ProjectSection.jsx
+++ src/components/ProjectSection.jsx
@@ -1,4 +1,7 @@
-
+import { useInView } from "framer-motion"
+import { useRef, useState } from "react"
+import ProjectTag from './ProjectTag'
+import ProjectCard from './ProjectCard'
 const projectsData = [
     {
         id: 1,
@@ -12,8 +15,34 @@ const projectsData = [
 ]
 
 const ProjectSection = () => {
+    const [tag, setTag] = useState('All')
+    const ref = useRef(null)
+    const isInView = useInView(ref, {once: true})
+    const handleTagChange = (newTag) => {
+        setTag(newTag)
+    }
+
+    const filteredProjects = projectData.filter((project) =>
+        project.tag.includes(tag)
+    )
+
+    const cardVariants = {
+        initial: { y: 50, opacity: 0},
+        animate: { y: 50, opacity: 1},
+    }
     return (
-        <div>ProjectSection</div>
+        <section id="projects">
+            <h2 className="text-center text-4xl font-bold text-white mt-4 mb-8 md:mb-12">
+                My Projects
+            </h2>
+            <div
+                className="text-white flex flex-row justify-center items-center gap-2 py-6"
+            >
+                <ProjectTag
+                    
+                />
+            </div>
+        </section>
     )
 }
 
